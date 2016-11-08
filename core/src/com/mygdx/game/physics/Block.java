52545310f38ced8364bc8b522b25452a5e7b8841
DaniProm <daniel.ruszin98@gmail.com>
2016-11-08 11:56:31 +0100

    csongi 2

--- core/src/com/mygdx/game/physics/Block.java
+++ core/src/com/mygdx/game/physics/Block.java
@@ -3,10 +3,12 @@ package com.mygdx.game.physics;
 /**
  * Created by Labor1PC20 on 2016.11.08..
  */
+
 public class Block {
 
     public Pont poz;
     public Pont meret;
+    public Pont irany;
 
     Block(float i1,float i2,float i3,float i4){
         poz = new Pont(i1,i2);
@@ -19,5 +21,9 @@ public class Block {
 
     public void setMeret(float f1,float f2) {
         this.meret = new Pont(f1,f2);
+    }
+
+    public void setIrany(float f1,float f2) {
+        this.irany = new Pont(f1,f2);
     }
 }
